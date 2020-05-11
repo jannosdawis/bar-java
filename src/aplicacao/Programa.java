e7cba55fc580b00ad26baa00cb59cdee4ee19b55
jannosdawis <jdawis@outlook.com>
2020-05-11 17:58:56 -0300

    Relatorio gerado

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -22,6 +22,32 @@ public class Programa {
 		System.out.print("Informe a quantidade de espetinhos: ");
 		espetinho = sc.nextInt();
 	
+    // CALCULOS
+		consumo = (5.00*cerveja)+(3.00*refrigerante)+(7.00*espetinho);
+		if (consumo >= 30.00){
+			couvert = 0.00;
+		}else{
+			couvert = 4.00;
+		}
+		if (sexo == 'F' || sexo == 'f') {
+			ingresso = 8.00;
+		}else{
+			ingresso = 10.00;
+		}
+		total = consumo + couvert + ingresso;
+
+	// RELATÓRIO
+		System.out.println("");
+		System.out.println("RELATÓRIO:");
+		System.out.printf("Consumo = R$ %.2f%n", consumo);
+		if (couvert == 0.0){
+			System.out.println("Isento de Couvert");
+		}else{
+			System.out.printf("Couvert = R$ %.2f%n", couvert);
+		}
+		System.out.printf("Ingresso = R$ %.2f%n", ingresso);
+		System.out.printf("Valor a pagar = R$ %.2f%n",total);
+
 		sc.close();
 	}
 }
